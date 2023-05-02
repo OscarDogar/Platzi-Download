2d3b6ba03bd92f8c19a48017b2293da0ea4d8882
OscarDogar <60854050+OscarDogar@users.noreply.github.com>
2023-05-01 22:32:54 -0500

    Now you can download the subtitles in some videos.

--- process.py
+++ process.py
@@ -1,36 +1,79 @@
 import subprocess
-from multiprocessing import Process
+# from multiprocessing import Process
 from multiprocessing import Pool
+import os
+import math
+import re
 
+def checkFolderExists (folder_path):
+    fullPath = os.getcwd() + folder_path
+    return os.path.exists(fullPath) and os.path.isdir(fullPath)
 
+def checkFileExists (file_path):
+    fullPath = os.getcwd() + file_path
+    return os.path.isfile(fullPath)
 
-def run_command(command):
+def run_command (command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stdout, stderr = process.communicate()
+    res1 = stdout.decode()
+    res2 = stderr.decode()
     exit_code = process.returncode
     if exit_code == 0:
-        return (f'Conversion succeeded for {command}')
+        if "failed: Error number -138 occurred" in stderr.decode():
+            return (f'Conversion failed with exit code {exit_code} for {command}')
+        else:
+            return (f'Conversion succeeded for {command}')
     else:
         return (f'Conversion failed with exit code {exit_code} for {command}')
     
 def createCommands (info):
     commands = []
     folder = 'cd videos &&'
-    i = 0
     for key, value in info.items():
-        commands.append('{} ffmpeg -i {} -c copy "{}.mp4"'.format(folder, value, key))
+        if not checkFileExists(f"\\videos\\{key}.mp4"):
+            commands.append('{} ffmpeg -i {} -c copy "{}.mp4"'.format(folder, value, key))
+        else:
+            print(f"The file {key} already exists")
     return commands
 
-def callProcess(info):
+def downloadSubs (subtitles):
+    regex = r"\-[a-zA-Z]{2,3}\-" 
+    anotherRegex = r"\.[a-zA-Z]{2,3}\-" 
+    if not checkFolderExists("\\videos\\Subs"):
+        subprocess.run("cd videos && mkdir Subs", shell=True)
+    for key, value in subtitles.items():
+        for sub in value:
+            name = key
+            #region Get the language of the subtitle
+            match = re.search(regex, sub) or re.search(anotherRegex, sub)
+            if match:
+                language = sub[match.start() + 1:match.end() - 1]
+                if language == "sp":
+                    language = "spa"
+            else:
+                print(f"No match found for {sub}")
+            #endregion
+            name = name + f".{language}.vtt"
+            subprocess.run(f'cd videos/Subs && curl {sub} -o "{name}"', shell=True)
+
+def callProcess(info, subtitles):
+    #current working directory
     commands = createCommands(info)
-    subprocess.run("mkdir videos", shell=True)
-    subprocess.run("cd videos", shell=True)
+    if len(subtitles) > 0:
+        downloadSubs(subtitles)
+    else:
+        print("No subtitles to download")
+    if not checkFolderExists("\\videos"):
+        subprocess.run("mkdir videos", shell=True)
     # Create a pool of 3 worker processes
     processNumber = 3
     pool = Pool(processes=processNumber)
-    results = []
+    
+    print(f"{len(commands)} videos will be downloaded divided into groups of {processNumber} for a total of {math.ceil(len(commands)/3)} groups.")
     # Divide the commands into groups of processNumber and run them in parallel
     for i in range(0, len(commands), processNumber):
+        results = []
         group = commands[i:i+processNumber]
         results.append(pool.map(run_command, group))
         #check if the results have failed 
@@ -42,7 +85,7 @@ def callProcess(info):
         print(f"Group {i//processNumber + 1} completed with {count} failed conversions")
 
     # Close the pool and wait for all processes to complete
-    print(results)
+    # print(results)
     pool.close()
     pool.join()
 
